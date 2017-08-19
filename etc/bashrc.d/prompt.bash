a3f202300d7be7fc5b8a5d2a373abe1ae0a04d78
GuyHoozdis <guy.hoozdis@gmail.com>
2017-08-19 16:10:16 -0500

    Add AppEnv to PS1 and refactor components

--- etc/bashrc.d/prompt.bash
+++ etc/bashrc.d/prompt.bash
@@ -15,9 +15,9 @@
     PROMPT_DIRTRIM=3
 
 
-# Git Prompt Helper
+# Prompt Helpers
 
-    _git_current_branch_for_prompt() {
+    _get_current_branch_name_for_prompt() {
       local ACTIVE_BRANCH=$(git branch --no-color 2>/dev/null | grep '^\* ' | sed 's/^* //g')
       if [ -z ${ACTIVE_BRANCH} ]; then
         echo "Not a git repository"
@@ -26,34 +26,45 @@
       fi
     }
 
+    _get_sourced_environment_name_for_prompt() {
+      echo -n "${APP_ENVIRONMENT-System}"
+    }
+
 
 # Manage the components independently to be more readable.
 #
 # Notice the leading backslash on the command substitution below,
 # where the branch name is calculated`\$(git ...`, without that
 # backslash the expression is only evaluated once.
+#
+# TODO: Wishlist
+#  * I'd like to change the color or styling of elements based on attributes of
+#    that element; for example, the git branch can be red (or underlined) when it
+#    has changes and green otherwise.
+#  * Right now I have the elements stacked, which is fine when I'm on big screens, but
+#    I'd like to be able to collapse or toggle the elements easily and as needed.
 
-    HOSTNAME="${WHITE}[${GREEN}\w${WHITE}]${NIL}"
-    VIRTENV="${WHITE}(${CYAN}PyEnv: ${RED}\$(pyenv version-name)${WHITE})"
-    BRANCH="\$(_git_current_branch_for_prompt)"
-    GIT="${WHITE}(${CYAN}Branch: ${RED}${BRANCH}${WHITE})${NIL}"
-    # XXX: If you want to add this to the prompt, you need to evaluate the variable
-    # everytime (like $BRANCH) not just when this file is sourced.
-    #APP_ENV="${WHITE}(${CYAN}AppEnv: ${RED} ${APP_ENVIRONMENT-System}${WHITE})${NIL}"
+    HOST_INFO="TODO"
+    HOST_INFO_PROMPT="${WHITE}[${GREEN}\w${WHITE}]${NIL}"
 
+    VIRTENV_NAME="TODO"
+    VIRTENV_PROMPT="${WHITE}(${CYAN}PyEnv: ${RED}\$(pyenv version-name)${WHITE})"
 
-# Put it together and what have you gut!?!  Something that is not
-# very good looking... but it is functional.  Meh, I've been running
-# with no color in my prompt for the last year and a half and this is
-# incrementally better than that.  I think I'm going to look seriously
-# at powerline/airline again.
+    BRANCH_NAME="\$(_get_current_branch_name_for_prompt)"
+    BRANCH_NAME_PROMPT="${WHITE}(${CYAN}Branch: ${RED}${BRANCH}${WHITE})${NIL}"
+
+    APP_ENV_NAME="\$(_get_sourced_environment_name_for_prompt)"
+    APP_ENV_PROMPT="${WHITE}(${CYAN}AppEnv: ${RED}${APP_ENV_NAME}${WHITE})${NIL}"
 
     function set_prompt() {
         PS1="\n"
-        PS1+="$HOSTNAME\n"
-        PS1+="$GIT $VIRTENV\n"
+        PS1+="$APP_ENV_PROMPT\n"
+        PS1+="$VIRTENV_PROMPT\n"
+        PS1+="$BRANCH_NAME_PROMPT\n"
+        PS1+="$HOST_INFO_PROMPT\n"
         PS1+="\$ "
 
         export PS1
     }
+
     export PROMPT_COMMAND=set_prompt
