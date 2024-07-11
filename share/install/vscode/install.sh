136a43b627de93e440677bb59f940e12696835b6
Guy Hoozdis <GuyHoozdis@users.noreply.github.com>
2024-07-11 15:08:29 -0500

    Create test installer

---
+++ share/install/vscode/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+## Install dotfiles inside VSCode Devcontainer
+#
+# Docs: https://code.visualstudio.com/docs/devcontainers/containers#_personalizing-with-dotfile-repositories
+# Reference GuyHoozdis/devcontainer-experiment
+
+echo "[*] Installing dotfiles"
+echo "[-] CWD: ${PWD}"
+
