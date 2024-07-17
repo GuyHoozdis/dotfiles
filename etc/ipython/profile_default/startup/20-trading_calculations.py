0a4276479bbab695f9e93baeea92607348e43e0e
Guy Hoozdis <GuyHoozdis@users.noreply.github.com>
2024-07-17 11:53:28 -0500

    Some reorganization of utility functions

--- etc/ipython/profile_default/startup/20-trading_calculations.py
+++ etc/ipython/profile_default/startup/20-trading_calculations.py
@@ -1,6 +1,8 @@
 """Utilities used for trading.
 """
+from argparse import Namespace
 from collections import namedtuple
+from math import floor
 
 
 ExpectedMove = namedtuple('ExpectedMove', ['upper', 'close', 'lower'])
@@ -57,3 +59,94 @@ def calc_in_out_spread_return(cost):
         key: cost * value
         for key, value in percentages.items()
     }
+
+
+def calc_position_risk(equity, cost):
+    """Determine the number of units of the given position to take.
+
+    A conservative tolerance would is 2%.  An agressive tolerance is 4% or 5%.
+    Anything beyond is generally considered wreckless.  These risk tolerance
+    levels are designed to maximize the survivability of your account in the
+    event of everything going wrong.  Taking beyond a 5% risk for any single
+    position is to be wreckless.  That's gambling.  Not math.
+
+    If the result of this function says 0 for your risk tolerance, then you
+    cannot take the position.
+
+    Args:
+        equity: The total liquidated value of the account.
+        cost: The debit for one unit of the position being analyzed.
+
+    Example:
+        >>> equity = 113041.66
+        >>> trading_utils.position_risk(equity, 113)
+        {'1%': 10, '2%': 20, '3%': 30, '4%': 40, '5%': 50}
+
+        >>> risk_profile = trading_utils.position_risk(equity, 2_000)
+        >>> risk_profile['2%']
+        1
+        >>> print(risk_profile)
+        {'1%': 0, '2%': 1, '3%': 1, '4%': 2, '5%': 2}
+
+        >>> risk_profile = trading_utils.position_risk(equity, 2_500)
+        >>> risk_profile['2%']
+        0
+        >>> print(risk_profile)
+        {'1%': 0, '2%': 0, '3%': 1, '4%': 1, '5%': 2}
+
+    """
+    def _risk(tolerance):
+        return floor((_D(equity) * _D(tolerance)) / _D(cost))
+
+    percentages = {
+        "1%": _D(.01),
+        "2%": _D(.02),
+        "3%": _D(.03),
+        "4%": _D(.04),
+        "5%": _D(.05),
+    }
+    return {
+        key: _risk(value)
+        for key, value in percentages.items()
+    }
+
+
+# #############################################################################
+# I want to make it easier to find these utilities instead of trying to recall
+# the name.
+# #############################################################################
+
+# Idea 1) Package them as a namespace.
+# - The Namespace is convoluted when you do things like '?' or '??' on the
+#   object.
+# - Maybe it would be better / cleaner if I just wrapped them in a class
+#   defined here.
+trading_utils_ns = Namespace(
+    expected_move=calc_expected_move,
+    vertical_spread_return=calc_in_out_spread_return,
+    position_risk=calc_position_risk,
+    __doc__="Simulate a module for trading utilties",
+)
+# -----------------------------------------------------------------------------
+
+# Idea 2) Package them as module.
+# - This works and behaves correctly for things like '?' and '??'.
+# - It needs to be imported once in the shell though.  Not sure if that is a
+#   good or bad thing, but it is different from how other startup scripts work.
+# - Maybe it would make more sense to make this an actual package / module and
+#   then this startup script could import it.
+import sys
+from types import ModuleType
+_trading_utils_module = ModuleType("trading_utils")
+sys.modules[_trading_utils_module.__name__] = _trading_utils_module
+_trading_utils_module.__file__ = __file__
+_trading_utils_module.__doc__ = __doc__
+
+_trading_utils_module.expected_move = calc_expected_move
+_trading_utils_module.vertical_spread_return = calc_in_out_spread_return
+_trading_utils_module.position_risk = calc_position_risk
+
+# !!!: I added a startup script that loads immediately after this one and does
+# the import <module>.  That seems to have worked, but it seems like a lot to
+# do to avoid making a real module / package.
+# -----------------------------------------------------------------------------
