656eaec121361e2a44848294bc0c70989c650fc3
Guy Hoozdis <GuyHoozdis@users.noreply.github.com>
2025-07-08 16:32:32 -0500

    Add object inspection utility

--- etc/ipython/profile_default/startup/17-pretty-print.py
+++ etc/ipython/profile_default/startup/17-pretty-print.py
@@ -6,3 +6,12 @@ from pprint import pp
 
 ppl = pp
 ppd = pp
+
+
+def keys(o):
+    if isinstance(o, dict):
+        return o.keys()
+    elif hasattr(o, '__dict__'):
+        return vars(o).keys()
+    raise TypeError(f"You haven't yet defined how to do this for type {type(o)}.")
+
