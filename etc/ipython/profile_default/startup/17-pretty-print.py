972d19456730a33cc0e8704a0447be594ef64595
Guy Hoozdis <GuyHoozdis@users.noreply.github.com>
2025-09-09 12:11:52 -0500

    Upgrade keys helper util

--- etc/ipython/profile_default/startup/17-pretty-print.py
+++ etc/ipython/profile_default/startup/17-pretty-print.py
@@ -8,10 +8,24 @@ ppl = pp
 ppd = pp
 
 
+# TODO: Consider fetching from __slots__ too.
+# !!!: The elif is not correct, as some objects have both __dict__
+#      and __slots__.  This is just for reference.
+#def get_from_dunder_dict(o):
+#    if hasattr(o, '__dict__'):
+#        return vars(o)
+#    elif hasattr(o, '__slots__'):
+#        return {k: getattr(o, k) for k in o.__slots__ if hasattr(o, k)}
+#    return {}
+
 def keys(o):
+    mapping = {}
     if isinstance(o, dict):
         return o.keys()
     elif hasattr(o, '__dict__'):
-        return vars(o).keys()
-    raise TypeError(f"You haven't yet defined how to do this for type {type(o)}.")
+        mapping = vars(o)
 
+    if not mapping and hasattr(o, '__dir__'):
+        mapping = {k: None for k in dir(o) if not k.startswith('_')}
+
+    return mapping.keys()
